6b98cfb816d30c4e30b20151e4bdefbbf18c4f3a
Elbong Gearny <elbong@navelgazer.club>
2021-09-09 19:32:02 -0700

    Set up .env w/initial values for Notion access
    Note: in case you're unfamiliar with the `dotenv` package, to test this out, you need a .env file (ignored by git) with contents that look something like:
    
    NOTION_API_KEY=secret_fG4u…ZgD1
    NOTION_DATABASE_ID=a828…gRpb

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Greetings, Planet! 👽");
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const apiKey = process.env.NOTION_API_KEY;
+const dbId = process.env.NOTION_DATABASE_ID;
+
+console.log("Greetings, Planet! 👽", ` Here are your apiKey and dbId, respectively: ${apiKey} ${dbId}`);
