9783580653c590a2b5aefd06e5654eec20fa8c46
Elbong Gearny <elbong@navelgazer.club>
2021-09-15 22:07:02 -0700

    MVP stack test from Notion to SvelteKit app
    MVP stack test from Notion to SvelteKit app
    
    # How to demo current progress (as it currently stands)
    
    ## Step 1 — Set the Target
    
    Have a target destination ready. Since notion2svelte is an active project, My setup looks something like this:
    
    ```
    ~/
    └── projects/
        └── notion2svelte/
            └── .env
        └── myproj/
            └── src
                └── routes
                    └── posts   ← notion2svelte output will go in here
    ```
    
    To send notion2svelte’s output to the _posts_ dir, you might add this to your .env:
    
    _notion2svelte/.env_
    ```
    OUTPUT_PATH=~/projects/myproj/src/routes/posts/
    ```
    
    ## Step 2 — Set the Source
    
    This is more complicated, and I don’t have the bandwidth to descibe it in full, but here’s the gist.
    
    - Create a database in Notion and get its ID from the URL (it’s the hexadecimal string between / and ?)
    - Create a Status column
    - Add a page to this database and give it a name
    - Set the page’s Status to "3. Ready to Publish"
    
    ## Step 3 - Connect notion2svelte to the Notion API
    
    Again just summarizing for now:
    
    - Follow Notion’s steps to set up API access, involving creating some sort of service endpoint thing and then, using the database’s Share features in Notion, give that endpoint access to the db
    - Edit notion2svelte’s _.env_ with 2 new values:
    
    _notion2svelte/.env_
    ```
    NOTION_API_KEY=secret_Qh1…
    NOTION_DATABASE_ID=c317…
    OUTPUT_PATH=~/projects/myproj/src/routes/posts/
    ```
    
    ## Run the script
    
    ```
    cd notion2svelte
    node index.js
    ```

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-import log from './lib/log.js'
-import { fetchAllPages, fetchPageBlocks, pageTitle, plain } from './lib/notion.js'
+import fs from 'fs'
+import { fetchAllPages, pageTitle, plain, slug } from './lib/notion.js'
+import { resolveTilde } from './lib/resolve-tilde.js'
+import env from './env.js'
+
+const out = env.OUTPUT_PATH;
 
 fetchAllPages().then(pages => {
-  const title = plain(pageTitle(pages[0]))
-  console.log(
-    `<h1>${title}</h1>`
-  )
+  for (let page of pages) {
+    const title = plain(pageTitle(page))
+    const path = resolveTilde(out) + slug(page) + '.svelte'
+
+    let contents = `<svelte:head>\n  <title>About</title>\n</svelte:head>`
+    contents += `<h1>${title}</h1>\n`;
+
+    fs.writeFile(path, contents, (err) => {
+      if (err) throw err;
+
+      console.log(`File saved to ${path}\n↓\n`, contents);
+    });
+  }
 })
