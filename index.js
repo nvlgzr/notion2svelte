ab53f8e40871ea2b3fdbe174f0a9ec59b64f7bf2
Elbong Gearny <elbong@navelgazer.club>
2021-09-15 23:31:56 -0700

    First (incomplete) stab at `fromNotion()`
    …whose goal is to recursively traverse all blocks and spit out Svelte-ready html

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 import fs from 'fs'
-import { fetchAllPages, pageTitle, plain, slug } from './lib/notion.js'
+import { fetchAllPages, fetchPageBlocks, pageTitle, plain, slug } from './lib/notion.js'
 import { resolveTilde } from './lib/resolve-tilde.js'
 import h from './lib/sveltifier.js'
 import env from './env.js'
 
 const out = env.OUTPUT_PATH;
+const styles = fs.readFileSync('./styles.css', { encoding: 'utf8' })
 
 fetchAllPages().then(pages => {
   for (let page of pages) {
-    const title = plain(pageTitle(page))
-    const path = resolveTilde(out) + slug(page) + '.svelte'
-    let contents = h.headTitle(title) + h._ + h.title(title)
+    fetchPageBlocks(page).then(blocks => {
+      const title = plain(pageTitle(page))
+      const path = resolveTilde(out) + slug(page) + '.svelte'
+      let contents = h.headTitle(title) + h._ + h.title(title)
 
-    contents += '…🐌'
+      for (let block of blocks) {
+        try {
+          contents += h.fromNotion(block) + h.___
+        } catch (error) {
+          if (block.type !== 'unsupported') {
+            // This isn't worth reporting as Notion simply, well
+            // doesn't support some blocks yet.
+          } else {
+            console.log(
+              `🚨 Error → ${error} | block → ${JSON.stringify(block)}`
+            )
+          }
+        }
+      }
 
-    fs.writeFile(path, contents, (err) => {
-      if (err) throw err;
+      contents += h._ + `<style>\n${styles}\n</style>`;
 
-      console.log(`File saved to ${path}\n↓\n`, contents);
-    });
+      fs.writeFile(path, contents, (err) => {
+        if (err) throw err;
+
+        console.log(`File saved to ${path}\n↓\n`, contents);
+      });
+    })
   }
 })
