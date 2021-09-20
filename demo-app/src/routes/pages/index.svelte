0b94856afba25ff63cf4088490de4cbf232a9018
Elbong Gearny <elbong@navelgazer.club>
2021-09-19 21:38:43 -0700

    Added a fairly useless index to the `pages` route
    Motivation: I find it comforting to have _something_ at the root of any given route. It's just more friendly, when your fingers happen to navigate to `localhost:3000/pages`, to not get a 404, and I don't have a good place to redirect to, just yet.

---
+++ demo-app/src/routes/pages/index.svelte
@@ -0,0 +1,43 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+</script>
+
+<svelte:head>
+	<title>Notion Pages</title>
+</svelte:head>
+
+<section>
+	<h1>Notion Pages</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
