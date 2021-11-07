b74d0a1aedc81f1ef67f6bd644323f08ba4dca97
Elbong Gearny <elbong@navelgazer.club>
2021-11-06 22:41:55 -0700

    First pass at "Magic" component, designed to apply a global transformationm to all blocks
    In this case, the global transformation is that every block gets displayed alongside the JSON that generated it.
    
    ⚠️ Note that I tried this on the Bells & Whistles page, and got a "Maximum call stack size exceeded" error from the compiler. 😳

---
+++ demo-app/src/lib/notion2svelte/Magic.svelte
@@ -0,0 +1,41 @@
+<script>
+	// Sky's the limit to what you can do with these outer components,
+	// which get reference to the raw page and the entire block, just
+	// in case YOU need something that's not on MY radar.
+
+	export let page;
+	export let block;
+	export let show; // Show JSON
+</script>
+
+{#if show}
+	<div>
+		<slot />
+	</div>
+{:else}
+	<div class="side-by-side">
+		<div class="fancy-card">
+			<slot />
+			<pre>
+{block}
+      </pre>
+		</div>
+	</div>
+{/if}
+
+<style>
+	div {
+		margin: 0;
+		padding: 0;
+	}
+
+	.fancy-card {
+		display: grid;
+		background-color: #fff;
+		border-radius: 4px;
+		box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+		transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+		margin-bottom: 20px;
+		padding: 20px;
+	}
+</style>
