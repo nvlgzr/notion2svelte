18fc54accab43d9a87e4e6de5146937aa17de26d
Elbong Gearny <elbong@navelgazer.club>
2021-11-09 13:38:26 -0800

    Added Nav link to /pages

--- demo-app/src/lib/header/Header.svelte
+++ demo-app/src/lib/header/Header.svelte
@@ -16,6 +16,9 @@
 		</svg>
 		<ul>
 			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.path === '/pages'}>
+				<a sveltekit:prefetch href="/pages">notion2svelte</a>
+			</li>
 			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
 			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
 		</ul>
