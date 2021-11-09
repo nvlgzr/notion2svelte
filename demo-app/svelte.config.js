f49eaf3c6bb76ccb2d9005749e398b936c828972
Elbong Gearny <elbong@navelgazer.club>
2021-11-09 15:21:55 -0800

    Added adapter-static
    `npm i -D @sveltejs/adapter-static@next` + config
    
    To test from within _demo-app_, `npm run build && cd build && serve`

--- demo-app/svelte.config.js
+++ demo-app/svelte.config.js
@@ -1,9 +1,21 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+import adapter from '@sveltejs/adapter-static';
+
+const handleError = ({ status, path, referrer, referenceType }) => {
+  if (path.startsWith('/blog')) throw new Error('Missing a blog page!');
+  console.warn(`${status} ${path}${referrer ? ` (${referenceType} from ${referrer})` : ''}`);
+};
+
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: adapter(),
+    prerender: {
+      onError: handleError
+    }
+  }
 };
 
 export default config;
