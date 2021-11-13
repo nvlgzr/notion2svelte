4004d2d94d44e6c5979a30a33e8fca8dec31a4a6
Elbong Gearny <elbong@navelgazer.club>
2021-11-13 03:44:28 -0800

    minor tweak to InlineCode styling

--- demo-app/src/lib/notion2svelte/InlineCode.svelte
+++ demo-app/src/lib/notion2svelte/InlineCode.svelte
@@ -4,18 +4,19 @@
 	span {
 		font-family: 'SFMono-Regular', Menlo, Consolas, 'PT Mono', 'Liberation Mono', Courier, monospace;
 		line-height: normal;
-		background: rgba(135, 131, 120, 0.15);
+		background: hsla(0, 0%, 100%, 0.45);
 		color: #eb5757;
 		border-radius: 3px;
 		font-size: 85%; /* Sets height of gray box */
-		padding: 0.2em 0.4em;
+		padding: 0.1rem 0.2rem 0.1rem 0.2rem;
 	}
 
 	pre {
 		display: inline;
+		background: hsla(0, 0%, 100%, 0.75);
 		color: inherit;
 		font-size: 85%;
 		white-space: pre-wrap;
-		padding: 0.7rem 0.1rem 0.6rem 0.1rem;
+		padding: 0.4rem 0.1rem 0.3rem 0.1rem;
 	}
 </style>
