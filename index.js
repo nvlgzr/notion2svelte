7fae302c38b0f461a814df223911ef8f96ea6b86
Elbong Gearny <elbong@navelgazer.club>
2021-12-13 17:06:17 -0800

    pure refactor: autoformat

--- index.js
+++ index.js
@@ -1,104 +1,102 @@
 #!/usr/bin/env node
 
 import { promises as fs } from "fs";
-import { join } from 'path'
-import { exec } from 'child_process'
-import { fetchAllPages, fetchFullPage } from './lib/notion.js'
-import { resolveTilde } from './lib/resolve-tilde.js'
-import { renderPage, stripDashes, slugFrom } from './lib/sveltifier.js'
-import env from './env.js'
+import { join } from "path";
+import { exec } from "child_process";
+import { fetchAllPages, fetchFullPage } from "./lib/notion.js";
+import { resolveTilde } from "./lib/resolve-tilde.js";
+import { renderPage, stripDashes, slugFrom } from "./lib/sveltifier.js";
+import env from "./env.js";
 
-const out = resolveTilde(env.OUTPUT_PATH)
-const testPageId = env.NOTION_TEST_PAGE_ID
-const db = env.NOTION_DATABASE_ID
-const cacheToken = env.CACHE_TOKEN
-const ignoreCache = env.FORCE_REFRESH
-const titleField = env.PAGE_TITLE_FIELD || "Name"
-const publishableStatus = env.PUBLISHABLE_STATUS || "Publishable"
+const out = resolveTilde(env.OUTPUT_PATH);
+const testPageId = env.NOTION_TEST_PAGE_ID;
+const db = env.NOTION_DATABASE_ID;
+const cacheToken = env.CACHE_TOKEN;
+const ignoreCache = env.FORCE_REFRESH;
+const titleField = env.PAGE_TITLE_FIELD || "Name";
+const publishableStatus = env.PUBLISHABLE_STATUS || "Publishable";
 
 function go() {
   if (testPageId) {
-    runTest()
+    runTest();
   } else {
-    run()
+    run();
   }
 }
 
 async function run() {
-  console.log('🏭')
+  console.log("🏭");
 
-  let pages = [''];
+  let pages = [""];
 
   try {
-    console.log(`1. Fetching publishable pages from Database #${db}`)
+    console.log(`1. Fetching publishable pages from Database #${db}`);
     pages = await fetchAllPages({
       dbId: db,
-      publishableStatus
-    })
-  }
-  catch (e) {
-    console.error('💥🙉 Abort! Abort!', e)
-    return
+      publishableStatus,
+    });
+  } catch (e) {
+    console.error("💥🙉 Abort! Abort!", e);
+    return;
   }
 
-  console.log(`2. Processing ${pages.length} ${pages.length === 1 ? 'page' : 'pages'}`)
+  console.log(
+    `2. Processing ${pages.length} ${pages.length === 1 ? "page" : "pages"}`
+  );
   for (let page of pages) {
-    const pageId = stripDashes(page.id)
-    const slug = slugFrom(page)
+    const pageId = stripDashes(page.id);
+    const slug = slugFrom(page);
 
-    console.log(" › ———")
-    console.log(` › Fetching #${pageId}`)
-    const path = join(out, slug + '.svelte')
-    const fullPage = await fetchFullPage(pageId)
+    console.log(" › ———");
+    console.log(` › Fetching #${pageId}`);
+    const path = join(out, slug + ".svelte");
+    const fullPage = await fetchFullPage(pageId);
 
-    const jsonPath = join(out, slug + '.json')
-    console.log(` › Writing JSON to ${jsonPath}`)
-    await fs.writeFile(jsonPath, JSON.stringify(fullPage, null, 2))
+    const jsonPath = join(out, slug + ".json");
+    console.log(` › Writing JSON to ${jsonPath}`);
+    await fs.writeFile(jsonPath, JSON.stringify(fullPage, null, 2));
 
-    console.log(` › Rendering…`)
-    const contents = renderPage(fullPage, titleField)
+    console.log(` › Rendering…`);
+    const contents = renderPage(fullPage, titleField);
 
-    console.log(` › Writing rendered page to #${path}`)
-    await fs.writeFile(path, contents)
+    console.log(` › Writing rendered page to #${path}`);
+    await fs.writeFile(path, contents);
 
-    console.log(` › Formatting file…`)
+    console.log(` › Formatting file…`);
 
-    if (path.includes(' ')) {
+    if (path.includes(" ")) {
       console.warn(
         "\n⚠️ 🐌\n\n  ⎸ Looks like you've got a space in your slug,\n  ⎸ which might be a mistake.\n  ⎸\n  ⎸ Usually `my-page-slug` or `MyPageSlug` is\n  ⎸ preferable to `My Page Slug`, since that\n  ⎸ translates to `…/My%20Page%20Slug. \n  ⎸\n"
       );
     }
-    exec(
-      `prettier --write "${path}"`,
-      (e, stdout, stderr) => {
-        if (e) {
-          console.log(` › error: ${e.message}`);
+    exec(`prettier --write "${path}"`, (e, stdout, stderr) => {
+      if (e) {
+        console.log(` › error: ${e.message}`);
 
-          return;
-        }
-        if (stderr && !stderr.includes('Debugger attached')) {
-          console.log(` › stderr: ${stderr}`);
-          return;
-        }
-        // console.log(` › ${stdout.trim()}`);
-      });
+        return;
+      }
+      if (stderr && !stderr.includes("Debugger attached")) {
+        console.log(` › stderr: ${stderr}`);
+        return;
+      }
+      // console.log(` › ${stdout.trim()}`);
+    });
   }
-  console.log(`⟢ FIN ⟣\n`)
+  console.log(`⟢ FIN ⟣\n`);
 }
 
 async function runTest() {
-  console.log('🐞', testPageId)
+  console.log("🐞", testPageId);
 
-  const testPagePath = join(out, 'test.json')
-  let pageJSON = ''
+  const testPagePath = join(out, "test.json");
+  let pageJSON = "";
 
   try {
-    console.log(`0. Checking for existing test cache…`)
-    const cache = await fs.readFile(testPagePath, "utf-8")
-    console.log(`1. Cache found. Parsing.`)
-    pageJSON = JSON.parse(cache)
-  }
-  catch (e) {
+    console.log(`0. Checking for existing test cache…`);
+    const cache = await fs.readFile(testPagePath, "utf-8");
+    console.log(`1. Cache found. Parsing.`);
+    pageJSON = JSON.parse(cache);
+  } catch (e) {
     // Usually just means the file's not there, which is expected
     // behavior, e.g., on first run, so there's no point spamming
     // the console.
@@ -107,17 +105,18 @@ async function runTest() {
 
   if (!pageJSON || testPageId !== cacheToken || ignoreCache) {
     try {
-      console.log(`1a. Cache empty or broken. Fetching fresh page data for #${testPageId}`)
-      pageJSON = await fetchFullPage(testPageId)
-    }
-    catch (e) {
-      console.error('💥🙉 Abort! Abort!', e, testPageId)
-      return
+      console.log(
+        `1a. Cache empty or broken. Fetching fresh page data for #${testPageId}`
+      );
+      pageJSON = await fetchFullPage(testPageId);
+    } catch (e) {
+      console.error("💥🙉 Abort! Abort!", e, testPageId);
+      return;
     }
 
     try {
-      console.log('1b. Writing to test cache.')
-      await fs.writeFile(testPagePath, JSON.stringify(pageJSON, null, 2))
+      console.log("1b. Writing to test cache.");
+      await fs.writeFile(testPagePath, JSON.stringify(pageJSON, null, 2));
       exec(
         `perl -pi.bak -e 's/CACHE_TOKEN=.*/CACHE_TOKEN=${testPageId}/g' .env; rm .env.bak`,
         (e, stdout, stderr) => {
@@ -130,33 +129,34 @@ async function runTest() {
             return;
           }
           console.log(`1b. ${stdout.trim()}`);
-        });
-    }
-    catch (e) {
-      console.warn(`FYI: Unable to save cache to ${testPagePath}: ${e}`)
+        }
+      );
+    } catch (e) {
+      console.warn(`FYI: Unable to save cache to ${testPagePath}: ${e}`);
     }
   }
 
-  let renderedPage = '';
+  let renderedPage = "";
 
   try {
-    console.log(`2. Rendering data`)
-    renderedPage = renderPage(pageJSON, titleField)
-  }
-  catch (e) {
-    console.log(`💥🐛 Mayday! Mayday!:\n\n${e}\n\njson ↴\n${JSON.stringify(pageJSON)}\n\n`)
-    return
-  }
-
-  try {
-    const sveltePath = join(out, 'test.svelte')
-    console.log(`3. Writing rendered Svelte page to ${sveltePath}`)
-    await fs.writeFile(sveltePath, renderedPage)
-    console.log(`⟢ FIN ⟣\n`)
+    console.log(`2. Rendering data`);
+    renderedPage = renderPage(pageJSON, titleField);
   } catch (e) {
-    console.log(`⚰️ Dead at the finish line: ${e}`)
-    return
+    console.log(
+      `💥🐛 Mayday! Mayday!:\n\n${e}\n\njson ↴\n${JSON.stringify(pageJSON)}\n\n`
+    );
+    return;
+  }
+
+  try {
+    const sveltePath = join(out, "test.svelte");
+    console.log(`3. Writing rendered Svelte page to ${sveltePath}`);
+    await fs.writeFile(sveltePath, renderedPage);
+    console.log(`⟢ FIN ⟣\n`);
+  } catch (e) {
+    console.log(`⚰️ Dead at the finish line: ${e}`);
+    return;
   }
 }
 
-go()
+go();
