901249c71d69d0e12a79092003240c500d6011fa
Elbong Gearny <elbong@navelgazer.club>
2021-12-13 17:06:33 -0800

    some null check fixes

--- demo-app/src/routes/pages/__layout.svelte
+++ demo-app/src/routes/pages/__layout.svelte
@@ -4,7 +4,10 @@
 	// exists as its own library for use by notion2svelte AND
 	// directly within projects?
 	export const pageTitle = (page) => page.properties.Title.title;
-	export const slug = (page) => page.properties.Slug[page.properties.Slug.type][0].plain_text;
+	export const slug = (page) =>
+		page.properties.Slug[page.properties.Slug.type]?.length
+			? page.properties.Slug[page.properties.Slug.type][0].plain_text
+			: '';
 	export const plain = (title) => title.reduce((acc, curr, idx) => acc + curr.plain_text, '');
 	export const plainText = (blockExtras) => blockExtras.map((s) => s.plain_text).join('');
 
