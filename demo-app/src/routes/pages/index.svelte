e468ec1588fc0afda2fc4d0260aec9873132fb8c
Elbong Gearny <elbong@navelgazer.club>
2022-01-10 03:52:48 -0800

    Reset demo-app to Svelte Kit skeleton, "fully loaded"

--- demo-app/src/routes/pages/index.svelte
+++
@@ -1,8 +0,0 @@
-<script context="module">
-	export async function load() {
-		return {
-			status: 302,
-			redirect: 'pages/about-notion2svelte'
-		};
-	}
-</script>
--- demo-app/svelte.config.js
+++ demo-app/svelte.config.js
@@ -1,21 +1,18 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
 /** @type {import('@sveltejs/kit').Config} */
-
-import adapter from '@sveltejs/adapter-static';
-
-const handleError = ({ status, path, referrer, referenceType }) => {
-  if (path.startsWith('/blog')) throw new Error('Missing a blog page!');
-  console.warn(`${status} ${path}${referrer ? ` (${referenceType} from ${referrer})` : ''}`);
-};
-
 const config = {
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-    adapter: adapter(),
-    prerender: {
-      onError: handleError
-    }
-  }
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
 };
 
 export default config;
