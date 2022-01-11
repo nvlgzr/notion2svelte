bfa7405e33bf57f9ff01202b9b5305d409d491c0
Elbong Gearny <elbong@navelgazer.club>
2022-01-10 22:54:47 -0800

    Tighter code styles
    Problem: Code in a two-column layout looked shite because the "main width" used here (that calc thing with all the notes about duplication) carries fewer characters per line than Notion's design. Shrinking the text and reducing whitespace helps in this case.
    
    Of course, so would incorporating Highlight.js or the like, but we're keeping it simple for now!

--- demo-app/src/lib/notion2svelte/Code.svelte
+++ demo-app/src/lib/notion2svelte/Code.svelte
@@ -18,20 +18,21 @@
 		line-height: normal;
 		display: block;
 		border-radius: 0.5rem;
-		margin: 0 2.5rem;
-		padding: 2.5rem;
+		margin: 0 1.5rem;
+		padding: 1.5rem;
 		overflow-x: auto;
 		white-space: pre-wrap;
 		word-wrap: break-word;
 		background: hsl(135, 88%, 16%);
 		color: hsl(120, 100%, 50%);
+		font-size: 0.75rem;
 	}
 
 	span {
 		position: absolute;
 		right: 1rem;
 		bottom: 1rem;
-		font-size: 0.8em;
+		font-size: 0.8rem;
 		color: hsl(120, 50%, 50%);
 	}
 </style>
