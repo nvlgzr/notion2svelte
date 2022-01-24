ca47f3f51560df89ca151b1a7dcbd69ed4b793b4
Elbong Gearny <elbong@navelgazer.club>
2022-01-23 17:43:36 -0800

    Shortened vertical padding in InlineCode prevents overlap when a bit of inline code on one line meets inline code on the line below

--- demo-app/src/lib/notion2svelte/InlineCode.svelte
+++ demo-app/src/lib/notion2svelte/InlineCode.svelte
@@ -7,7 +7,6 @@
 <style>
   span {
     font-family: 'SFMono-Regular', Menlo, Consolas, 'PT Mono', 'Liberation Mono', Courier, monospace;
-    line-height: normal;
     background: hsla(0, 0%, 100%, 0.45);
     color: hsl(268, 71%, 43%);
     border-radius: 3px;
@@ -22,6 +21,6 @@
     border-radius: 3px;
     font-size: 85%;
     white-space: pre-wrap;
-    padding: 0.4rem 0.5rem 0.3rem 0.5rem;
+    padding: 0.2rem 0.5rem 0.2rem 0.5rem;
   }
 </style>
