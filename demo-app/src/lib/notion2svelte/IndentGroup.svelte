0232f36343b5bb643f5c2bd47e04062e0a7123d0
Elbong Gearny <elbong@navelgazer.club>
2022-01-26 15:42:44 -0800

    Removed unused border CSS
    This border isn't rendering, nor do I want it to. Seems like a very old artifact.

--- demo-app/src/lib/notion2svelte/IndentGroup.svelte
+++ demo-app/src/lib/notion2svelte/IndentGroup.svelte
@@ -1,9 +1,8 @@
 <div><slot /></div>
 
 <style>
-	div {
-		padding-left: 1.5rem;
-		border: solid double 0.5px wheat;
-		/* background: radial-gradient(coral, wheat); */
-	}
+  div {
+    padding-left: 1.5rem;
+    /* background: radial-gradient(coral, wheat); */
+  }
 </style>
