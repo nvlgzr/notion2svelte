6d81d24d76fc31fcc25894bf68f0173287da5dfa
Elbong Gearny <elbong@navelgazer.club>
2022-04-20 12:01:40 -0700

    Refactor index.js: extract file system functions

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-import { promises as fs, existsSync, mkdirSync } from 'fs';
-import { join, dirname } from 'path';
+import { fs, join } from './lib/fs-extras.js';
 import { exec } from 'child_process';
 import { fetchAllPages, fetchFullPage } from './lib/notion.js';
 import { resolveTilde } from './lib/resolve-tilde.js';
@@ -22,15 +21,6 @@ function go() {
   } else {
     runTest();
   }
-}
-
-function ensureDirectoryExistence(filePath) {
-  var dir = dirname(filePath);
-  if (existsSync(dir)) {
-    return true;
-  }
-  ensureDirectoryExistence(dir);
-  mkdirSync(dir);
 }
 
 function stripExpiryTimes(pageJSON) {
