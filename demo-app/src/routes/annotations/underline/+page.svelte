33e585bb1699885290ef3a7d2ccf64789d054d70
Elbong Gearny <elbong@navelgazer.club>
2022-12-07 21:07:37 -0800

    Added docs for Underline

---
+++ demo-app/src/routes/annotations/underline/+page.svelte
@@ -0,0 +1,271 @@
+<script>
+	import Error from '$lib/notion2svelte/Error.svelte';
+	import Cover from '$lib/notion2svelte/Cover.svelte';
+	import Title from '$lib/notion2svelte/Title.svelte';
+	import BulletedListItem from '$lib/notion2svelte/BulletedListItem.svelte';
+	import Callout from '$lib/notion2svelte/Callout.svelte';
+	import ChildPage from '$lib/notion2svelte/ChildPage.svelte';
+	import Code from '$lib/notion2svelte/Code.svelte';
+	import ColumnList from '$lib/notion2svelte/ColumnList.svelte';
+	import Column from '$lib/notion2svelte/Column.svelte';
+	import Divider from '$lib/notion2svelte/Divider.svelte';
+	import Embed from '$lib/notion2svelte/Embed.svelte';
+	import Header from '$lib/notion2svelte/Header.svelte';
+	import Image from '$lib/notion2svelte/Image.svelte';
+	import IndentGroup from '$lib/notion2svelte/IndentGroup.svelte';
+	import Underline from '$lib/notion2svelte/Underline.svelte';
+	import InlineCode from '$lib/notion2svelte/InlineCode.svelte';
+	import InlineColor from '$lib/notion2svelte/InlineColor.svelte';
+	import Equation from '$lib/notion2svelte/Equation.svelte';
+	import Link from '$lib/notion2svelte/Link.svelte';
+	import NumberedListItem from '$lib/notion2svelte/NumberedListItem.svelte';
+	import Paragraph from '$lib/notion2svelte/Paragraph.svelte';
+	import Quote from '$lib/notion2svelte/Quote.svelte';
+	import TodoItem from '$lib/notion2svelte/TodoItem.svelte';
+	import Toggle from '$lib/notion2svelte/Toggle.svelte';
+	import { setContext } from 'svelte';
+
+	setContext('pageStuff', {
+		pageId: '5cb76e2e6d8f4697b53a95905d6c0c69',
+		title: 'Underline'
+	});
+</script>
+
+<svelte:head>
+	<title>Underline</title>
+</svelte:head>
+
+<Title>Underline</Title>
+
+<Header level={1}>1. Notion original</Header>
+<Image url="/assets/annotations/underline/d6a96338-17a7-4fde-a5e4-bfedf86b0d5b.png" />
+<Header level={1}>2. Notion API</Header>
+<Paragraph
+	blockProps={{
+		pageId: '5cb76e2e6d8f4697b53a95905d6c0c69',
+		id: '0538fec5-3bc6-4c24-815d-78b5ce846819',
+		created_time: '2022-01-28T20:26:00.000Z',
+		last_edited_time: '2022-12-06T22:00:00.000Z'
+	}}
+>
+	<InlineColor value="gray">Partial output of&ensp;</InlineColor><InlineColor value="gray"
+		><em>src/routes/[slug]/notion-export.js</em></InlineColor
+	>
+</Paragraph>
+<Code
+	code={`\{
+  &quot;object&quot;: &quot;block&quot;,
+  …
+  &quot;paragraph&quot;: \{
+    &quot;text&quot;: [
+      \{
+        …
+        &quot;plain_text&quot;: &quot;In general, the use of &quot;,
+        …
+      },
+      \{
+        &quot;type&quot;: &quot;text&quot;,
+        &quot;text&quot;: \{
+          &quot;content&quot;: &quot;underlining&quot;,
+          &quot;link&quot;: null
+        },
+        &quot;annotations&quot;: \{
+          …
+          &quot;underline&quot;: true,
+          …
+        },
+        &quot;plain_text&quot;: &quot;underlining&quot;,
+        &quot;href&quot;: null
+      },
+      \{
+        …
+        &quot;plain_text&quot;: &quot;is as &quot;,
+        …
+      },
+      …
+    ]
+  }
+}`}
+	language="javascript"
+/><Header level={1}>3. Svelte output</Header>
+<Paragraph
+	blockProps={{
+		pageId: '5cb76e2e6d8f4697b53a95905d6c0c69',
+		id: '28871a46-c697-4500-b517-1c526797ab38',
+		created_time: '2022-01-28T20:32:00.000Z',
+		last_edited_time: '2022-01-28T20:32:00.000Z'
+	}}
+>
+	<InlineColor value="gray">Partial output of&ensp;</InlineColor><InlineColor value="gray"
+		><em>src/routes/[slug].svelte</em></InlineColor
+	>
+</Paragraph>
+<Code
+	code={`&lt;Paragraph blockProps=\{\{…}}&gt;
+	In general, the use of &lt;Underline&gt;underlining&lt;/Underline&gt; is as &lt;Link…&gt;contraindicated&lt;/Link&gt; as the passive voice
+&lt;/Paragraph&gt;`}
+	language="html"
+/><Header level={1}>4. Example rendering</Header>
+<Paragraph
+	blockProps={{
+		pageId: '5cb76e2e6d8f4697b53a95905d6c0c69',
+		id: 'f6e54e55-1799-4733-8776-a7ee7c81518c',
+		created_time: '2022-12-08T04:39:00.000Z',
+		last_edited_time: '2022-12-08T04:39:00.000Z'
+	}}
+>
+	Green squiggly rendered by&ensp;<Link
+		type="absolute"
+		href="https://github.com/nvlgzr/notion2svelte/blob/main/demo-app/src/lib/notion2svelte/Underline.svelte"
+		><InlineColor value="gray">Underline.svelte</InlineColor></Link
+	>
+</Paragraph>
+<Paragraph
+	blockProps={{
+		pageId: '5cb76e2e6d8f4697b53a95905d6c0c69',
+		id: '0498c749-a331-4f3d-9965-b4a71209d7b8',
+		created_time: '2022-12-08T04:39:00.000Z',
+		last_edited_time: '2022-12-08T04:46:00.000Z'
+	}}
+>
+	In general, the use of&ensp;<Underline>underlining</Underline>&ensp;is as&ensp;<Link
+		type="absolute"
+		href="https://practicaltypography.com/underlining.html">contraindicated</Link
+	>&ensp;as is the passive voice.
+</Paragraph>
+<Divider />
+<Header level={2}
+	><Link type="alias" href="/about-notion2svelte">🏠</Link>&ensp;Browse the docs ⚘&ensp;</Header
+>
+<ColumnList cols={2}
+	><Column
+		><Header level={3}>High-level Discussion</Header>
+		<BulletedListItem
+			><Link type="mention" href="/high-level-discussion/flow-diagram"
+				>Where notion2svelte Fits</Link
+			>&ensp;</BulletedListItem
+		>
+		<BulletedListItem><Link type="mention" href="/components">Components</Link></BulletedListItem>
+		<BulletedListItem
+			><Link type="mention" href="/high-level-discussion/rules-for-writing-good-slugs"
+				>Writing Good Slugs</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/high-level-discussion/block-props">blockProps</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/high-level-discussion/nested-blocks">Nested Blocks</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/high-level-discussion/synced-blocks">Synced Blocks</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/high-level-discussion/dot-env">Configuring your .env</Link
+			>&ensp;</BulletedListItem
+		>
+		<Header level={3}>Turn-intoable Block Components</Header>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/paragraph">Paragraph (aka “Text”)</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/header">Header (3 types in 1!)</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/child-page">ChildPage</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/todo-item">TodoItem</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/bulleted-list-item">BulletedListItem</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/numbered-list-item">NumberedListItem</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/toggle">Toggle</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/code">Code</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/quote">Quote</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/callout">Callout</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/equation">Equation</Link></BulletedListItem
+		>
+		<BulletedListItem
+			>Toggle Headings<InlineColor value="gray">&ensp;(not yet implemented)</InlineColor
+			></BulletedListItem
+		>
+	</Column><Column
+		><Header level={3}>Layout-only Components</Header>
+		<BulletedListItem
+			><Link type="mention" href="/layout-only-components/indent-group">IndentGroup</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/layout-only-components/columns">Column & ColumnList</Link
+			>&ensp;</BulletedListItem
+		>
+		<Header level={3}>Page-level Components</Header>
+		<BulletedListItem
+			><Link type="mention" href="/page-level-components/title">Title (aka Name)</Link
+			></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/page-level-components/cover">Cover (Image & Icon)</Link
+			></BulletedListItem
+		>
+		<Header level={3}>Annotation Components</Header>
+		<BulletedListItem
+			><strong>bold</strong>&ensp;→&ensp;<InlineCode
+				code={'&lt;strong&gt;'}
+			/>&ensp;</BulletedListItem
+		>
+		<BulletedListItem
+			><em>italic</em>&ensp;→&ensp;<InlineCode code={'&lt;em&gt;'} /></BulletedListItem
+		>
+		<BulletedListItem
+			><s>strikethrough</s>&ensp;→&ensp;<InlineCode code={'&lt;s&gt;'} /></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/annotations/underline">Underline</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/annotations/inline-code">InlineCode</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/annotations/inline-color">InlineColor</Link></BulletedListItem
+		>
+		<BulletedListItem><Link type="mention" href="/annotations/link">Link</Link></BulletedListItem>
+		<BulletedListItem
+			><Link type="mention" href="/standard-components/equation">Equation</Link></BulletedListItem
+		>
+		<Header level={3}>Other Components</Header>
+		<BulletedListItem
+			><Link type="mention" href="/other-components/divider">Divider</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/other-components/embed">Embed</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/other-components/image">Image</Link></BulletedListItem
+		>
+		<BulletedListItem
+			><Link type="mention" href="/other-components/error">Error</Link></BulletedListItem
+		>
+	</Column></ColumnList
+>
